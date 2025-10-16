a610065e0d3aa29e756d1de00afc0a611a0f5dce
Marco <marcoantonio.sanchez@iesdonana.org>
2025-10-16 19:41:58 +0200

    Refactorizacion

--- borrar.php
+++ borrar.php
@@ -5,6 +5,7 @@ require 'auxiliar.php';
 
 $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT, FILTER_NULL_ON_FAILURE);
 
+
 if ($id) {
     $pdo = conectar();
     $sent = $pdo->prepare("DELETE FROM clientes WHERE id = :id");
